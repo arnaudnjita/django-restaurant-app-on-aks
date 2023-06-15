dd5d124f2393356c20f1b41d6df7de269495977c
Njita Arnaud <arnaudnjita@gmail.com>
2023-06-15 03:24:41 +0100

    Updated my project app settings

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 if 'CODESPACE_NAME' in os.environ:
     CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
@@ -90,7 +89,7 @@ DATABASES = {
 
 
 # Configure Postgres database for local development
-# Set these environment variables in the .env file for this project.
+#   Set these environment variables in the .env file for this project.
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -98,8 +97,6 @@ DATABASES = {
 #         'HOST': os.environ.get('DBHOST'),
 #         'USER': os.environ.get('DBUSER'),
 #         'PASSWORD': os.environ.get('DBPASS'),
-#         'PORT':'5432',
-#         'OPTIONS': {'sslmode': 'require'}
 #     }
 # }
 
