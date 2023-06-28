64af2255338c590ccaeddb5b669fdd8bcd10a8c3
arnaudnjita <arnaudnjita@gmail.com>
2023-06-28 15:20:19 +0100

    updated requirements.txt

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'azureproject.wsgi.application'
 #   Set these environment variables in the .env file for this project.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DBNAME'),
         'HOST': os.environ.get('DBHOST'),
         'USER': os.environ.get('DBUSER'),
